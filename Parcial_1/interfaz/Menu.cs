d557f43de2eaa3f5cf8615aaf70764caee3a41a4
Carpediem-code234 <camilo6mendoza@gmail.com>
2024-03-19 14:36:40 -0500

    se le hizo cambios en el menu

--- Parcial_1/interfaz/Menu.cs
+++ Parcial_1/interfaz/Menu.cs
@@ -10,10 +10,10 @@ namespace Parcial_1.interfaz
 {
     public class Menu
     {
+        List<Grupo> grupos = new List<Grupo>();
         public void Menuprincipal()
         {
             bool salir = false;
-            List<Grupo> grupos = new List<Grupo>();
             
 
             while (!salir)
@@ -31,126 +31,143 @@ namespace Parcial_1.interfaz
                 switch (opcion)
                 {
                     case "1":
-                        Console.Clear();
-                        Console.Write("Ingrese el nombre del grupo: ");
-                        string nombreGrupo = Console.ReadLine();
-                        Grupo nuevoGrupo = new Grupo(nombreGrupo);
-                        grupos.Add(nuevoGrupo);
-                        Console.WriteLine($"Grupo '{nombreGrupo}' creado.");
-                        Console.ReadKey();
+                        CrearGrupo();
                         break;
                     case "2":
-                        if (grupos.Count == 0)
-                        {
-                            Console.WriteLine("No hay grupos creados. Cree un grupo primero.");
-                            Console.ReadKey();
-                            break;
-                        }
-                        Console.Clear();
-                        Console.WriteLine("Grupos disponibles:");
-                        for (int i = 0; i < grupos.Count; i++)
-                        {
-                            Console.WriteLine($"{i + 1}. {grupos[i].NombreGrupo}");
-                        }
-                        Console.Write("Seleccione el número de grupo al que desea agregar un estudiante: ");
-                        int indiceGrupo = int.Parse(Console.ReadLine()) - 1;
-
-                        Console.Write("Ingrese el nombre del estudiante: ");
-                        string nombreEstudiante = Console.ReadLine();
-                        Console.Write("Ingrese el apellido del estudiante: ");
-                        string apellidoEstudiante = Console.ReadLine();
-                        Console.Write("Ingrese la edad del estudiante: ");
-                        int edadEstudiante = int.Parse(Console.ReadLine());
-                        Console.Write("Ingrese el sexo del estudiante (M/F): ");
-                        string sexoEstudiante = Console.ReadLine();
-                        Console.Write("Ingrese el promedio de notas del estudiante: ");
-                        double promedioNotasEstudiante = double.Parse(Console.ReadLine());
-
-                        Estudiante nuevoEstudiante = new Estudiante(nombreEstudiante, apellidoEstudiante, edadEstudiante, sexoEstudiante, promedioNotasEstudiante);
-                        grupos[indiceGrupo].AgregarEstudiante(nuevoEstudiante);
-                        Console.ReadKey();
+                        AddEstudiante();
                         break;
                     case "3":
-                        if (grupos.Count == 0)
-                        {
-                            Console.WriteLine("No hay grupos creados. Cree un grupo primero.");
-                            Console.ReadKey();
-                            break;
-                        }
-                        Console.Clear();
-                        Console.WriteLine("Grupos disponibles:");
-                        for (int i = 0; i < grupos.Count; i++)
-                        {
-                            Console.WriteLine($"{i + 1}. {grupos[i].NombreGrupo}");
-                        }
-                        Console.Write("Seleccione el número de grupo del que desea ver los estudiantes: ");
-                        int indiceGrupoEstudiantes = int.Parse(Console.ReadLine()) - 1;
-                        grupos[indiceGrupoEstudiantes].ImprimirEstudiantes();
-                        Console.ReadKey();
+                        EstudianteEnGrupo();
                         break;
                     case "4":
-                        
-                        if (grupos.Count < 2)
-                        {
-                            Console.WriteLine("Debe haber al menos dos grupos para realizar operaciones entre grupos.");
-                            Console.ReadKey();
-                            break;
-                        }
-                        Console.Clear();
-                        Console.WriteLine("Grupos disponibles:");
-                        for (int i = 0; i < grupos.Count; i++)
-                        {
-                            Console.WriteLine($"{i + 1}. {grupos[i].NombreGrupo}");
-                        }
-                        Console.Write("Seleccione el número del primer grupo: ");
-                        int indiceGrupo1 = int.Parse(Console.ReadLine()) - 1;
-                        Console.Write("Seleccione el número del segundo grupo: ");
-                        int indiceGrupo2 = int.Parse(Console.ReadLine()) - 1;
-
-                        Console.WriteLine("Seleccione la operación:");
-                        Console.WriteLine("1. Unión");
-                        Console.WriteLine("2. Intersección");
-                        Console.WriteLine("3. Diferencia (M - N)");
-                        string operacion = Console.ReadLine();
-
-                        OperacionesGrupo operaciones = new OperacionesGrupo();
-                        Grupo resultadoOperacion = null;
-                        switch (operacion)
-                        {
-                            case "1":
-                                
-                                resultadoOperacion = operaciones.Union(grupos[indiceGrupo1], grupos[indiceGrupo2]);
-                                break;
-                            case "2":
-                                
-                                resultadoOperacion = operaciones.Interseccion(grupos[indiceGrupo1], grupos[indiceGrupo2]);
-                                break;
-                            case "3":
-                                resultadoOperacion = operaciones.Diferencia(grupos[indiceGrupo1], grupos[indiceGrupo2]);
-                                break;
-                            default:
-                                Console.WriteLine("Opción no válida.");
-                                break;
-                        }
-
-                        if (resultadoOperacion != null)
-                        {
-                            Console.WriteLine("Resultado de la operación:");
-                            resultadoOperacion.ImprimirEstudiantes();
-                        }
-                        Console.ReadKey();
+                        OperacionesEntreGrupos();
                         break;
                     case "5":
                         salir = true;
                         break;
                     default:
-                        Console.WriteLine("Opción no válida.");
+                        Console.WriteLine("Opción no válida. hacer el favor de seleccione una opción válida.");
 
                         break;
                 }
 
-                Console.WriteLine();
             }
+
+        }
+        public void CrearGrupo()
+        {
+            Console.Clear();
+            Console.Write("Ingrese el nombre del grupo: ");
+            string nombreGrupo = Console.ReadLine();
+            Grupo nuevoGrupo = new Grupo(nombreGrupo);
+            grupos.Add(nuevoGrupo);
+            Console.WriteLine($"Grupo '{nombreGrupo}' creado.");
+            Console.ReadKey();
+        }
+        public void AddEstudiante()
+        {
+            if (grupos.Count == 0)
+            {
+                Console.WriteLine("No hay grupos creados. Cree un grupo primero.");
+                Console.ReadKey();
+                return;
+            }
+            Console.Clear();
+            Console.WriteLine("Grupos disponibles:");
+            for (int i = 0; i < grupos.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {grupos[i].NombreGrupo}");
+            }
+            Console.Write("Seleccione el número de grupo al que desea agregar un estudiante: ");
+            int indiceGrupo = int.Parse(Console.ReadLine()) - 1;
+
+            Console.Write("Ingrese el nombre del estudiante: ");
+            string nombreEstudiante = Console.ReadLine();
+            Console.Write("Ingrese el apellido del estudiante: ");
+            string apellidoEstudiante = Console.ReadLine();
+            Console.Write("Ingrese la edad del estudiante: ");
+            int edadEstudiante = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el sexo del estudiante (M/F): ");
+            string sexoEstudiante = Console.ReadLine();
+            Console.Write("Ingrese el promedio de notas del estudiante: ");
+            double promedioNotasEstudiante = double.Parse(Console.ReadLine());
+
+            Estudiante nuevoEstudiante = new Estudiante(nombreEstudiante, apellidoEstudiante, edadEstudiante, sexoEstudiante, promedioNotasEstudiante);
+            grupos[indiceGrupo].AgregarEstudiante(nuevoEstudiante);
+            Console.ReadKey();
+        }
+        public void EstudianteEnGrupo()
+        {
+            if (grupos.Count == 0)
+            {
+                Console.WriteLine("No hay grupos creados. Cree un grupo primero.");
+                Console.ReadKey();
+                return;
+            }
+            Console.Clear();
+            Console.WriteLine("Grupos disponibles:");
+            for (int i = 0; i < grupos.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {grupos[i].NombreGrupo}");
+            }
+            Console.Write("Seleccione el número de grupo del que desea ver los estudiantes: ");
+            int indiceGrupoEstudiantes = int.Parse(Console.ReadLine()) - 1;
+            grupos[indiceGrupoEstudiantes].ImprimirEstudiantes();
+            Console.ReadKey();
+        }
+        public void OperacionesEntreGrupos()
+        {
+
+            if (grupos.Count < 2)
+            {
+                Console.WriteLine("Debe haber al menos dos grupos para realizar operaciones entre grupos.");
+                Console.ReadKey();
+                return;
+            }
+            Console.Clear();
+            Console.WriteLine("Grupos disponibles:");
+            for (int i = 0; i < grupos.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {grupos[i].NombreGrupo}");
+            }
+            Console.Write("Seleccione el número del primer grupo: ");
+            int indiceGrupo1 = int.Parse(Console.ReadLine()) - 1;
+            Console.Write("Seleccione el número del segundo grupo: ");
+            int indiceGrupo2 = int.Parse(Console.ReadLine()) - 1;
+
+            Console.WriteLine("Seleccione la operación:");
+            Console.WriteLine("1. Unión");
+            Console.WriteLine("2. Intersección");
+            Console.WriteLine("3. Diferencia (M - N)");
+            string operacion = Console.ReadLine();
+
+            OperacionesGrupo operaciones = new OperacionesGrupo();
+            Grupo resultadoOperacion = null;
+            switch (operacion)
+            {
+                case "1":
+
+                    resultadoOperacion = operaciones.Union(grupos[indiceGrupo1], grupos[indiceGrupo2]);
+                    break;
+                case "2":
+
+                    resultadoOperacion = operaciones.Interseccion(grupos[indiceGrupo1], grupos[indiceGrupo2]);
+                    break;
+                case "3":
+                    resultadoOperacion = operaciones.Diferencia(grupos[indiceGrupo1], grupos[indiceGrupo2]);
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
+
+            if (resultadoOperacion != null)
+            {
+                Console.WriteLine("Resultado de la operación:");
+                resultadoOperacion.ImprimirEstudiantes();
+            }
+            Console.ReadKey();
         }
     }
+
 }
